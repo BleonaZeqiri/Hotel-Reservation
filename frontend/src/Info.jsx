6cfc2f005c1ce6565bd55d470133c5fd3b08157a
Bleona <zbleona9@gmail.com>
2025-12-17 11:20:04 +0100

    added a new page

---
+++ frontend/src/Info.jsx
@@ -0,0 +1,10 @@
+export default function Info() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Hotel Information</h2>
+      <p>
+        This section will contain hotel details. More content will be added later.
+      </p>
+    </div>
+  );
+}
