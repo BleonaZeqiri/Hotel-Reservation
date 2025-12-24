11f451d177bc5c2284b433e8e2907e823dca6f79
Bleona <zbleona9@gmail.com>
2025-12-24 19:46:26 +0100

    refactor(frontend): restructure frontend and migrate to new setup

--- frontend/src/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer style={{ textAlign: "center", padding: 10, marginTop: 40 }}>
-      <p>© 2024 Hotel Reservation System</p>
-    </footer>
-  );
-}
--- frontend/src/Sidebar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Sidebar() {
-  return (
-    <aside style={{ padding: 20 }}>
-      <h3>Sidebar</h3>
-      <p>Menu items will be added here.</p>
-    </aside>
-  );
-}
